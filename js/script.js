28bf724dca64d9d2f32345c32b0726d838ebd74a
Dave-93 <difrancesco.davide93@gmail.com>
2024-12-28 12:54:29 +0100

    feat: function chooseEvenOdd and fix others

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let inputWord = prompt("Inserisci una parola!");
 
 function palindromicWord(word){
-    inputWord = inputWord.toLowerCase();//Rende la parola minuscola  
-    let arrayWord = inputWord.split("");//Trasforma la stringa in array  
+    word = word.toLowerCase();//Rende la parola minuscola  
+    let arrayWord = word.split("");//Trasforma la stringa in array  
     let arrayReverseWord =  arrayWord.toReversed();//Crea una copia dell'array invertito  
     let reverseWord = arrayReverseWord.join("");//Trasforma l'array invertito in stringa 
 
-    if(inputWord === reverseWord){
+    if(word === reverseWord){
         alert("Parola Palindroma");
     }else{
         alert("Parola Non Palindroma");
@@ -14,27 +14,45 @@ function palindromicWord(word){
 }
 palindromicWord(inputWord);
 
-//let choose = prompt("Scegli pari o dispari");
+
+let inputChoose = prompt("Scegli pari o dispari");
+const even = "pari";
+const odd = "dispari";
+
+function chooseEvenOdd(choose){
+    choose = choose.toLowerCase();
+    //console.log(choose);
+    if(choose !== even && choose !== odd){//effettua controllo, se i valori sono diversi da even e odd restituisce alert
+        alert("Valore non corretto, Riprovare!")
+        location.reload();//todo Funziona solo se utilizzo questa funzione commentando le altre funzioni...Come posso far ricaricare la pagina senza che venga eseguita la funzione successiva??
+    }
+}
+chooseEvenOdd(inputChoose);
+
 
 let inputNumber = prompt ("Inserisci un numero da 1 a 5");
 
 function evenOdd(number){
-    inputNumber = parseInt(inputNumber);//Trasforma il numero inserito da string a number
-    console.log(inputNumber);
-    if(inputNumber >= 1 && inputNumber <= 5){
-        let randomNumber = Math.floor(Math.random()*5)+1;//math.random genera un num da 0 a 0,9999, math.floor arrotonda per difetto quindi moltiplicando * 5 avrò un range da 0 a 4, col +1 porto il range da 1 a 5
-        console.log(randomNumber);
+    number = parseInt(number);//Trasforma il numero inserito da string a number
+    if(number >= 1 && number <= 5){
+        let randomNumber = Math.floor(Math.random()*5)+1;//math.random genera un num da 0 a 0,9999, math.floor arrotonda per difetto quindi moltiplicando * 5 avrò un range da 0 a 4, col +1 porto il range da 1 a 5      
+        let somma = number + randomNumber;//Esegue la somma tra il numero dato in input e quello generato automaticamente
         
-        let somma = inputNumber + randomNumber;
-        console.log(somma);
-        
-        if(somma % 2 === 0){
-            alert("Numero Pari")
+        if(somma % 2 === 0){//Controlla se il numero è pari o dispari
+            alert(`Numero ${even}`);
         }else{
-            alert("Numero Dispari")
+            alert(`Numero ${odd}`);
         }
     }else{
-        alert("Numero non corretto, Riprovare!")
+        alert("Numero non corretto, Riprovare!")//alert se il num in input è minore di 1 o maggiore di 5
+        location.reload();//todo Qui funziona perchè è l'ultima funzione che viene eseguita
     }
 }
 evenOdd(inputNumber);
+
+ 
+//todo se chooseEvenOdd è uguale a evenOdd VINCO -> scelgo pari e dopo aver inserito un numero il totale esce pari
+//todo                                           -> scelgo dispari e dopo aver inserito un numero il totale esce dispari
+
+//todo se chooseEvenOdd è diverso a evenOdd PERDO -> scelgo pari e dopo aver inserito un numero il totale esce dispari
+//todo                                            -> scelgo dispari e dopo aver inserito un numero il totale esce pari
