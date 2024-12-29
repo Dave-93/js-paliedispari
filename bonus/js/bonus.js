0773bb2a62e96937b7056a9de321e9fe49dc9e8c
Dave-93 <difrancesco.davide93@gmail.com>
2024-12-29 17:11:15 +0100

    feat: add id bonus.html and variable assignment bonus.js

---
+++ bonus/js/bonus.js
@@ -0,0 +1,8 @@
+let inputWord = document.querySelector("#palindrome-input");
+const submitPalindrome = document.querySelector("#palindrome-button");
+let resultPalindrome = document.querySelector("#palindrome-result");
+
+submitPalindrome.addEventListener("click", function(event){
+    event.preventDefault();
+    
+})
