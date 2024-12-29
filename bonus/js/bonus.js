6afd1ebb38a1c584e2a4a1b06db9a8154cedb5b5
Dave-93 <difrancesco.davide93@gmail.com>
2024-12-29 20:31:42 +0100

    feat: add setTimeout and fix if control number

--- bonus/js/bonus.js
+++ bonus/js/bonus.js
@@ -14,7 +14,9 @@ submitPalindrome.addEventListener("click", function(event){
         
         if (!wordInput) {//controllo se l'utente non inserisce nulla(! viene usato per verificare se la variabile è "FALSY",se la stringa è vuota viene considerata "FALSY"...Quindi se l'utente non scrive nulla la condizione è VERA)
             resultPalindrome.innerHTML = "Per favore inserisci una parola e riprova!";
-            //location.reload();//todo mettere un timer per refresh
+            setTimeout(function() {
+                resultPalindrome.innerHTML = ""; // Pulisce il messaggio di errore dopo 3 secondi
+            }, 3000);
             return;
         }else if(word === reverseWord){
             resultPalindrome.innerHTML = "Complimenti, la parola inserita è Palindroma!";
@@ -34,33 +36,24 @@ submitEvenOdd.addEventListener("click", function(event){
     event.preventDefault();
 
     let selectInput =inputSelect.value;//Ottengo il valore dell'input
-    
-    console.log(selectInput);
-
     let numberInput =inputNumber.value;//Ottengo il valore dell'input
     
     function evenOdd(number){
         number = parseInt(number);//Trasforma il numero inserito da string a number
-
-        console.log(number);
-
-        if(number < 1 || number > 5){//Controlla se il numero è al di fuori del range da 1 a 5
-            resultEvenOdd.innerHTML = "Numero non corretto, Riprovare!";//alert se il num in input è minore di 1 o maggiore di 5
-            //location.reload();//todo mettere un timer
+        if(isNaN(number) || number < 1 || number > 5){//Controlla se il numero è al di fuori del range da 1 a 5 e se è stato inserito accidentalmente un carattere/parola 
+            resultEvenOdd.innerHTML = "Valore non corretto, Riprovare!";//alert se il num in input è minore di 1 o maggiore di 5
+            setTimeout(function() {
+                resultEvenOdd.innerHTML = ""; // Pulisce il messaggio dopo 3 secondi
+            }, 3000);
             return;//In questo caso il return serve perchè ferma l'esecuzione del codice successivo
         }else{
             let randomNumber = Math.floor(Math.random()*5)+1;//math.random genera un num da 0 a 0,9999, math.floor arrotonda per difetto quindi moltiplicando * 5 avrò un range da 0 a 4, col +1 porto il range da 1 a 5      
             let somma = number + randomNumber;//Esegue la somma tra il numero dato in input e quello generato automaticamente
-           
-            console.log(somma);
-           
             if(somma % 2 === 0){//Controlla se il numero è pari o dispari
                 somma = 0;
             }else{
                 somma = 1;
             }
-           
-            console.log(somma);
             
             if(somma == selectInput){//Confronta il risultato con la scelta dell'utente fatta nel select
                                      //todo Ho fatto la comparazione con 2 = invece di 3 perchè sono di tipo differente(somma->number, selectImput->string) anche se il valore è lo stesso
