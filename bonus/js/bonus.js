bd20b5bbc98934dce5dba6991e4fbc36c10a7ef5
Dave-93 <difrancesco.davide93@gmail.com>
2025-01-03 19:44:24 +0100

    feat: function timer

--- bonus/js/bonus.js
+++ bonus/js/bonus.js
@@ -13,15 +13,15 @@ submitPalindrome.addEventListener("click", function(event){
         let reverseWord = arrayReverseWord.join("");//Trasforma l'array invertito in stringa 
         
         if (!wordInput) {//controllo se l'utente non inserisce nulla(! viene usato per verificare se la variabile è "FALSY",se la stringa è vuota viene considerata "FALSY"...Quindi se l'utente non scrive nulla la condizione è VERA)
-            resultPalindrome.innerHTML = "Per favore inserisci una parola e riprova!";
-            setTimeout(function() {
-                resultPalindrome.innerHTML = ""; // Pulisce il messaggio di errore dopo 3 secondi
-            }, 3000);
+            resultPalindrome.innerHTML = "Per favore inserisci una parola e riprova! &#129335;";
+            timerPalindromeInputError()
             return;
         }else if(word === reverseWord){
-            resultPalindrome.innerHTML = "Complimenti, la parola inserita è Palindroma!";
+            resultPalindrome.innerHTML = "Complimenti, la parola inserita è Palindroma! &#129321";
+            timerPalindromeInputCorrect()
         }else{
-            resultPalindrome.innerHTML = "Mi dispiace, la parola inserita Non è Palindroma";
+            resultPalindrome.innerHTML = "Mi dispiace, la parola inserita Non è Palindroma &#128531;";
+            timerPalindromeInputCorrect()
         }
     }
     palindromicWord(wordInput);
@@ -40,19 +40,15 @@ submitEvenOdd.addEventListener("click", function(event){
     
     function evenOdd(number){
         if(selectInput !=0 && selectInput !=1){
-            resultEvenOdd.innerHTML = "Per favore effettua una scelta!";
-            setTimeout(function() {
-                resultEvenOdd.innerHTML = ""; // Pulisce il messaggio dopo 3 secondi
-            }, 3000);
+            resultEvenOdd.innerHTML = "Per favore effettua una scelta! &#129335;";
+            timerEvenOddInputError();
             return;
         }
         
         number = parseInt(number);//Trasforma il numero inserito da string a number
         if(isNaN(number) || number < 1 || number > 5){//Controlla se il numero è al di fuori del range da 1 a 5 e se è stato inserito accidentalmente un carattere/parola 
-            resultEvenOdd.innerHTML = "Valore non corretto, Riprovare!";//alert se il num in input è minore di 1 o maggiore di 5
-            setTimeout(function() {
-                resultEvenOdd.innerHTML = ""; // Pulisce il messaggio dopo 3 secondi
-            }, 3000);
+            resultEvenOdd.innerHTML = "Valore non corretto, Riprovare! &#129335;";//alert se il num in input è minore di 1 o maggiore di 5
+            timerEvenOddInputError();
             return;//In questo caso il return serve perchè ferma l'esecuzione del codice successivo
         }else{
             let randomNumber = Math.floor(Math.random()*5)+1;//math.random genera un num da 0 a 0,9999, math.floor arrotonda per difetto quindi moltiplicando * 5 avrò un range da 0 a 4, col +1 porto il range da 1 a 5      
@@ -65,13 +61,35 @@ submitEvenOdd.addEventListener("click", function(event){
             
             if(somma == selectInput){//Confronta il risultato con la scelta dell'utente fatta nel select
                                      //todo Ho fatto la comparazione con 2 = invece di 3 perchè sono di tipo differente(somma->number, selectImput->string) anche se il valore è lo stesso
-                resultEvenOdd.innerHTML = "CONGRATULAZIONI, HAI VINTO!!!";
+                resultEvenOdd.innerHTML = "CONGRATULAZIONI, HAI VINTO!!!  &#129321";
+                timerEvenOddInputCorrect()
             }else{
-                resultEvenOdd.innerHTML = "MI DISPIACE, HAI PERSO...";
+                resultEvenOdd.innerHTML = "MI DISPIACE, HAI PERSO...&#128531;";
+                timerEvenOddInputCorrect()
             }
         }
     }
     evenOdd(numberInput);
 })
 
-//todo Se metto un seTimeout su ogni "result" è meglio creare una funzione invece di ripeterlo n volte?
+function timerPalindromeInputError(){
+    setTimeout(function() {
+        resultPalindrome.innerHTML = ""; // Pulisce il messaggio dopo 3 secondi
+    }, 3000);
+}
+function timerPalindromeInputCorrect(){
+    setTimeout(function() {
+        resultPalindrome.innerHTML = ""; // Pulisce il messaggio dopo 5 secondi
+    }, 5000);
+}
+
+function timerEvenOddInputError(){
+    setTimeout(function() {
+        resultEvenOdd.innerHTML = ""; // Pulisce il messaggio dopo 3 secondi
+    }, 3000);
+}
+function timerEvenOddInputCorrect(){
+    setTimeout(function() {
+        resultEvenOdd.innerHTML = ""; // Pulisce il messaggio dopo 5 secondi
+    }, 5000);
+}
